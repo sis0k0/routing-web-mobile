25579ed90439373c418557a65f057fdbf88ad3cc
sis0k0 <stanimira.vlaeva@gmail.com>
2018-07-30 14:41:30 +0300

    remove web extension from home.component name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component.web';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
